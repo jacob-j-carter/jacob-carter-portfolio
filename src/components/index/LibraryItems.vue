bab064661aa2427be3021db4ed44f7ad65a9c4d8
Jacob Carter <jacob@choicedigital.studio>
2022-10-24 18:30:10 +0100

    Added links to portfolio items

--- src/components/index/LibraryItems.vue
+++ src/components/index/LibraryItems.vue
@@ -1,17 +1,19 @@
 <template>
 
   <div :key="project.name" v-for="project in portfolioItems" class="w-full border-b-2 border-b-solid border-b-white py-12">
-    <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'">
-    <h3 class="text-white font-size-56 my-4">
-      {{project.name}}
-    </h3>
-    <div class="flex justify-between items-center flex-wrap">
-      <p :key="tag.tag" v-for="tag in project.tags" class="text-stone-300 uppercase">
-        {{ tag.tag }}
-      </p>
-    </div>
+    <a :href="project.itemHREF">
+      <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'">
+      <h3 class="text-white font-size-56 my-4">
+        {{project.name}}
+      </h3>
+      <div class="flex justify-between items-center flex-wrap">
+        <p :key="tag.tag" v-for="tag in project.tags" class="text-stone-300 uppercase">
+          {{ tag.tag }}
+        </p>
+      </div>
+    </a>
   </div>
-
+  
 </template>
 
 
