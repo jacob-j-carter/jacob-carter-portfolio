2b3ab85aa2c5dae583ca5efff5fc4540716fd373
Jacob Carter <jacob@choicedigital.studio>
2022-10-24 12:35:41 +0100

    Added mailto link to button

--- src/components/index/Hero.vue
+++ src/components/index/Hero.vue
@@ -1,23 +1,29 @@
 <template>
 
-  <section class="mt-40 .wrapper px-4">
-		<h1 class="text-white">
-			<span id="header_jacob-carter">jacob carter </span>
-			<span>is a </span>
-			<span class="header-portion">digital developer </span>
-			<span>& </span>
-			<span class="header-portion">lover of coffee</span>
-		</h1>
-		<h2 class="sr-only">
-			jacob carter is a digital developer and lover of coffee
-		</h2>
+  <section class="mt-40">
+		
+    <article class="wrapper px-4">
+      <h1 class="text-white">
+        <span id="header_jacob-carter">jacob carter </span>
+        <span>is a </span>
+        <span class="header-portion">digital developer </span>
+        <span>& </span>
+        <span class="header-portion">lover of coffee</span>
+      </h1>
+      <h2 class="sr-only">
+        jacob carter is a digital developer and lover of coffee
+      </h2>
 
-    <button class="flex mt-12 font-size-28">
-      Say Hello
-      <div class="w-6 ml-4">
-        <img src="/assets/images/icons/handwave.svg" alt="hand waving icon" class="w-full" />
-      </div>
-    </button>
+      <a href="mailto:jacob@choicedigital.studio">
+        <button class="flex mt-20 font-size-28">
+          Say Hello
+          <div class="w-6 ml-4">
+            <img src="/assets/images/icons/handwave.svg" alt="hand waving icon" class="w-full" />
+          </div>
+        </button>
+      </a>
+
+    </article>
 
 	</section>
 
@@ -61,7 +67,9 @@ export default {
 
   h1:hover > .header-portion:hover {
     opacity: 1;
+    cursor:pointer;
   }
+
 
   #header_jacob-carter {
     opacity: 1;
