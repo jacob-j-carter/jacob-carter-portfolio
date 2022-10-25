d535d7f7b549410edfe721da5c678a223e7c6616
Jacob Carter <jacob@choicedigital.studio>
2022-10-25 14:52:12 +0100

    Added portfolio item page

---
+++ src/components/portfolio/CSSDisplayInfo.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <div>
+
+  </div>
+
+</template>
+
+
+<script lang="ts">
+import { Icon } from '@iconify/vue';
+
+export default {
+  name: 'Placeholder',
+  components: {
+		Icon,
+	},
+  // props: {
+
+  // },
+  // data () {
+  //   return {
+      
+  //   }
+  // },
+  // methods: {
+
+  // },
+  // created () {
+
+  // }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../../sass/abstracts' as *;
+  @use '../../sass/themes' as *;
+
+
+
+</style>
