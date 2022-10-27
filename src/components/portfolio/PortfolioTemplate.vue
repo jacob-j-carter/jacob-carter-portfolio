4d6cfdd73815939e570e0d3c4f2a784c354de278
Jacob Carter <jacob@choicedigital.studio>
2022-10-27 14:39:57 +0100

    Finished CSS Display Info page

--- src/components/portfolio/PortfolioTemplate.vue
+++ src/components/portfolio/PortfolioTemplate.vue
@@ -1,19 +1,19 @@
 <template>
 
-  <article class="wrapper px-4">
+  <article>
     <div class="w-full border-b-2 border-b-solid border-b-white py-4 mt-32">
-      <a :href="project.itemHREF">
-        <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'">
-        <h1 class="text-white my-4 font-size-56">
-          {{project.name}}
-        </h1>
-        <div class="flex justify-between items-center flex-wrap">
-          <p :key="tag.tag" v-for="tag in project.tags" class="text-stone-300 uppercase">
-            {{ tag.tag }}
-          </p>
-        </div>
-      </a>
+      <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'">
+      <h1 class="text-white my-4 font-size-56">
+        {{project.name}}
+      </h1>
+      <div class="flex justify-between items-center flex-wrap">
+        <p :key="tag.tag" v-for="tag in project.tags" class="text-stone-300 uppercase">
+          {{ tag.tag }}
+        </p>
+      </div>
     </div>
+
+    
   </article>
 
 </template>
@@ -33,6 +33,12 @@ export default {
     projectIndex: {
       type: Number,
       default: 0
+    },
+    goalContent: {
+      type: String,
+    },
+    challengesContent: {
+      type: String,
     }
   },
   data () {
