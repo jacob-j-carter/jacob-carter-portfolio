49b3daf81800a8a192f3b7d1d303a870940053aa
Jacob Carter <jacob@choicedigital.studio>
2022-10-27 23:23:03 +0100

    Added pill image component

---
+++ src/components/index/PillImage.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <img :src="'/assets/images/photos/pill-' + imgName + '.png'" :alt="imgAlt" class="inline mx-2 h-8">
+
+</template>
+
+
+<script lang="ts">
+import { Icon } from '@iconify/vue';
+
+export default {
+  name: 'PillImage',
+  components: {
+		Icon,
+	},
+  props: {
+    imgName: String,
+    imgAlt: String
+  },
+  // data () {
+  //   return {
+      
+  //   }
+  // },
+  // methods: {
+
+  // },
+  // created () {
+
+  // }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../../sass/abstracts' as *;
+  @use '../../sass/themes' as *;
+
+
+
+</style>
