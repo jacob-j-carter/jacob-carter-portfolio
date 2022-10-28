28e72d426b1efa6f267ab9914a59e89b54eb4a61
Jacob Carter <jacob@choicedigital.studio>
2022-10-28 14:23:38 +0100

    Added library hover effect

--- src/components/index/LibraryItems.vue
+++ src/components/index/LibraryItems.vue
@@ -1,9 +1,10 @@
 <template>
 
-  <div :key="project.name" v-for="project in portfolioItems" class="w-full border-b-2 border-b-solid border-b-white py-4 mt-8 md:mt-16">
-    <a :href="project.itemHREF" class="lg:flex lg:justify-between">
+  <div :key="project.name" v-for="project in portfolioItems" class="w-full border-b-2 border-b-solid border-b-white py-4 mt-8 md:mt-16" @mouseover="updateCoordinates">
+    <a :href="project.itemHREF" class="lg:flex lg:justify-between relative">
       <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'" class="md:h-96 w-full lg:hidden">
-      <h3 class="text-white font-size-48 my-4 lg:my-0">
+      <img :src="project.thumbnailURL" :alt="'mockup of' + project.name + 'website'" class="rounded-xl hidden absolute lg:h-48 lg:w-64 lg:block hover-img" :style="'top:' + (y * .05) + 'px;' + 'left:' + (x * .65) + 'px;'">
+      <h3 class="text-white font-size-48 my-4 lg:my-0" >
         {{project.name}}
       </h3>
       <div class="flex justify-between items-center lg:items-end flex-wrap lg:flex-nowrap">
@@ -29,14 +30,20 @@ export default {
   // props: {
 
   // },
-  data () {
+  data() {
     return {
+      x: 500,
+      y: 350,
+      cursorOpacity: 0,
       portfolioItems: portfolioItems
     }
   },
-  // methods: {
-
-  // },
+  methods: {
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+  }
   // created () {
 
   // }
@@ -49,6 +56,16 @@ export default {
   @use '../../sass/abstracts' as *;
   @use '../../sass/themes' as *;
 
+  .hover-img {
+    position: absolute;
+    translate: -50% -50%;
+    transition: top 400ms linear, left 400ms linear, opacity 150ms linear;
+    opacity: 0;
+    z-index: -1;
+  }
 
+  a:hover > .hover-img {
+    opacity: 1;
+  }
 
 </style>
