dd93035dddffd9d1ad45863ab37fb5959e16c5fd
Jacob Carter <jacob@choicedigital.studio>
2022-10-29 09:50:58 +0100

    Added social links to header

--- src/components/header/DesktopHeader.vue
+++ src/components/header/DesktopHeader.vue
@@ -7,8 +7,18 @@
         <h2 class="hidden md:flex uppercase text-white font-size-36 header-logo">Carter</h2>
       </a>
 
-      <div class="flex items-center justify-end text-white">
-        <a :key="page.pageName" v-for="page in pages" :href="page.pageHREF" class="button-font font-size-14 mx-6 hover:opacity-70" :class="[pageName == page.pageName ? 'line-through' : '']">{{ page.pageName }}</a>
+      <div class="flex">
+        <div class="flex items-center justify-end text-white">
+          <a :key="page.pageName" v-for="page in pages" :href="page.pageHREF" class="button-font font-size-14 mx-6 hover:opacity-70" :class="[pageName == page.pageName ? 'line-through' : '']">{{ page.pageName }}</a>
+        </div>
+        <div class="flex items-start">
+          <a href="https://github.com/Choice-Studio" target="_blank">
+            <Icon icon="akar-icons:github-fill" class="text-white hover:text-stone-400" />
+          </a>
+          <a href="https://github.com/Choice-Studio" target="_blank">
+            <Icon icon="akar-icons:linkedin-box-fill" class="text-white ml-4 hover:text-stone-400" />
+          </a>
+        </div>
       </div>
 
     </header>
