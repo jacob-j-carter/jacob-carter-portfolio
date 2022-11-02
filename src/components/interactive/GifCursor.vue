79a45501dc5cf9790189691428287b08eeb4283d
Jacob Carter <jacob@choicedigital.studio>
2022-11-02 23:12:42 +0000

    Changed development text

---
+++ src/components/interactive/GifCursor.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <img :src="cursorImg" alt="" @mousemove="updateCoordinates" :style="'top:' + y + 'px;' + 'left:' + x + 'px;'">
+  <h1 class="text-white">
+    {{ x }}
+  </h1>
+  <h1 class="text-white">
+    {{ y }}
+  </h1>
+
+</template>
+
+
+<script lang="ts">
+import { Icon } from '@iconify/vue';
+
+export default {
+  name: 'GifCursor',
+  components: {
+		Icon,
+	},
+  props: {
+    cursorImg: String
+  },
+  data() {
+    return {
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    updateCoordinates: function(event) {
+      // pass event object, bound to mouse move with updat
+      this.x = event.clientX;
+      this.y = event.clientY;
+    }
+  }
+  // created () {
+
+  // }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../../sass/abstracts' as *;
+  @use '../../sass/themes' as *;
+
+  img {
+    position: absolute;
+    width: 10em;
+    border-radius: 1em;
+    translate: -50% -50%;
+    top:50%;
+    left:50%;
+  }
+
+</style>
